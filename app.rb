43877f6292f07955497144e8b01f522c0b5489ae
Sameer Rahmani <lxsameer@gnu.org>
2014-06-06 15:04:50 +0430

    assets pack added

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
 
-get '/' do
-  # use the views/index.erb file
-  erb :'index.html'
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__) # You must set app root
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'app/assets/javascript'        # Default
+    serve '/css',    from: 'app/assets/stylesheets'       # Default
+    serve '/images', from: 'app/assets/images'    # Default
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :application, '/javascripts/application.js', [
+      '/js/vendor/**/*.js',
+      '/js/lib/**/*.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css'
+    ]
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :simple   # :simple | :sass | :yui | :sqwish
+  }
+
+  get '/' do
+    # use the views/index.erb file
+    erb :'index.html'
+  end
 end
