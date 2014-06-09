17e0ac6131abbc4349a06ce0a0e92475aec0f50e
Sameer Rahmani <lxsameer@gnu.org>
2014-06-09 17:40:08 +0430

    side menu added

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,43 @@
 require 'sinatra/base'
 require 'sinatra/asset_pipeline'
+require 'i18n'
+require 'i18n/backend/fallbacks'
 
 # Main Sinatra application class
 class RadioApp < Sinatra::Application
   set :root, File.dirname(__FILE__)
-  set :sessions, true
+
+  enable :sessions
+  enable :logging
+
+
+  configure do
+    I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+    I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+    I18n.backend.load_translations
+  end
 
   set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
   set :assets_prefix, %w(app/assets)
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
+
   register Sinatra::AssetPipeline
 
-
   @title = "RadioBoot"
+
+  helpers do
+    def t(*args)
+      # Just a simple alias
+      I18n.t(*args)
+    end
+
+  end
+
+  before '/[en|fa]:locale/*' do
+    I18n.locale       =       params[:locale]
+    request.path_info = '/' + params[:splat][0]
+  end
 
   get '/' do
     # use the views/index.erb file
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,2 @@
 //= require_tree ./lib
+//= require menu
