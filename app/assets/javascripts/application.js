311c2ec9af69d3a0e683bf02ac272d0dc1370982
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2014-06-24 16:11:16 +0430

    fullpage js added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
+//= require ./lib/jquery
 //= require_tree ./lib
 //= require menu
+$(document).ready(function() {
+    $('.body').fullpage({
+        css3: false,
+        scrollingSpeed: 700,
+        easing: 'easeInQuart'
+    });
+});
