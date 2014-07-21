2d27197ed5f770357c6b81b3dc7c537589ff2270
Sameer Rahmani <lxsameer@gnu.org>
2014-07-21 22:02:36 +0430

    new semantic layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,2 @@
 //= require ./lib/jquery
 //= require_tree ./lib
-//= require menu
-$(document).ready(function() {
-    $('.body').fullpage({
-        css3: false,
-        scrollingSpeed: 700,
-        easing: 'easeInQuart',
-        anchors: ['home', 'listen', 'sponsors']
-    });
-});
