60ceebd95c985585fe02e4fb1f5dde2923fa9789
Sameer Rahmani <lxsameer@gnu.org>
2014-08-04 18:10:33 +0430

    Playback lookup failed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,56 @@ $(function(){
         $(this).fadeOut();
     });
 
+    var json_data = $("#tmp").html().trim(),
+        details, lang, time_cache, timed_content;
+
+    console.log(json_data.trim().length);
+    if (json_data !== "") {
+        details = JSON.parse(json_data);
+        lang = $("html").attr('lang');
+        time_cache = [];
+        timed_content = {};
+
+        if ((details !== undefined) || (details != "")) {
+
+            var topics = details.topics.reverse();
+            $.each(topics, function(x){
+                var obj = details.topics[x];
+                $("#episode_menu").prepend("<a class='item' id='topic_" + obj.id + "' data-id='" + obj.id + "'>" + obj[lang] + "</a>");
+                time_cache.push(parseInt(obj.time));
+                console.log(details.sections[obj.id.toString()].desc[lang]);
+                timed_content["id_" + parseInt(obj.time).toString()] = [obj.id, details.sections[obj.id.toString()].desc[lang]];
+            });
+        }
+    }
+    $('video,audio').mediaelementplayer({
+        success: function (mediaElement, domObject) {
+
+            // add event listener
+            mediaElement.addEventListener('timeupdate', function(e) {
+                var ctime = parseInt(mediaElement.currentTime);
+                if (ctime in time_cache) {
+                    console.log('matched');
+                    console.log(timed_content);
+                    var c = timed_content["id_"+ ctime.toString()];
+                    console.log(c);
+                    console.log(timed_content.keys());
+                    $("#desc").html(cache[1]);
+                    $("#episode_menu .item").removeClass('active');
+                    $("#topic_" + cache[0]).addClass('active');
+                }
+            }, false);
+        },
+    });
+
+
+    $(".ui.dropdown").dropdown();
+    //$(".ui.computer.sidebar").sidebar('attach events', '.toggle.button.computer');
+    $(".ui.sidebar").sidebar({overlay: true}).sidebar('attach events', '.toggle.button');
+    $("#upload_file").on('click', function(event){
+        $("#actual_field").click();
+    });
+
     $("#subscribe").on('click', function(event){
         var email = $('#email').val();
         $(this).find('span')
@@ -57,40 +107,4 @@ $(function(){
 
     });
 
-    var details = JSON.parse($("#tmp").html());
-    $('video,audio').mediaelementplayer({
-        success: function (mediaElement, domObject) {
-
-            // add event listener
-            mediaElement.addEventListener('timeupdate', function(e) {
-                console.log(mediaElement.currentTime);
-                console.log(typeof(mediaElement.currentTime));
-            }, false);
-        },
-    });
-
-    $(".ui.dropdown").dropdown();
-    //$(".ui.computer.sidebar").sidebar('attach events', '.toggle.button.computer');
-    $(".ui.sidebar").sidebar({overlay: true}).sidebar('attach events', '.toggle.button');
-    $("#upload_file").on('click', function(event){
-        $("#actual_field").click();
-    });
-
-});
-
-$(function(){
-    var details = JSON.parse($("#tmp").html());
-    var lang = $("html").attr('lang');
-
-    if ((details !== undefined) || (details != "")) {
-
-        var topics = details.topics.reverse();
-        $.each(topics, function(x){
-            var obj = details.topics[x];
-            $("#episode_menu").prepend("<a class='item' id='topic_" + obj.id + "' data-id='" + obj.id + "'>" + obj[lang] + "</a>");
-
-        });
-
-
-    }
 });
